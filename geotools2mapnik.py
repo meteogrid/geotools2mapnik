3cb08ba090f238215c71c7ec89d0a307f5b61b1c
Alberto Valverde <alberto@meteogrid.com>
2012-11-28 14:46:58 +0100

    some cleanup and raster colorizer support

--- geotools2mapnik.py
+++ geotools2mapnik.py
@@ -5,9 +5,14 @@ import sys
 import optparse
 import tempfile
 import shlex
+import logging
+
 import mapnik2 as mapnik
+
 from lxml import etree
 from lxml import objectify
+
+log = logging.getLogger(__name__)
 
 FIX_HEX = False
 
@@ -163,8 +168,19 @@ def fix_colors(tree):
 
 def main(root,**options):
     m = mapnik.Map(1,1)
+
+    datasource = options.get('datasource')
+    if datasource and datasource.endswith('shp'):
+        shp_dir = os.path.abspath(datasource).split('.shp')[0]
+        name = datasource.split('.shp')[0]
+        datasource = mapnik.Shapefile(file=shp_dir)
+        if srid is not None:
+            m.srs = '+init=epsg:%s' % srid
+        else:
+            srs = proj4_from_osr(shp_dir)
+            if srs:
+                m.srs = srs
     
-    idx = 0
     
     layers = []
     if hasattr(root,'NamedLayer'):
@@ -172,110 +188,14 @@ def main(root,**options):
     if hasattr(root,'UserLayer'):
         layers.extend(root.UserLayer)
     for layer in layers:
-        lyr = mapnik.Layer(str(getattr(layer,'Name',None) or 'Layer'))
-        datasource = options.get('datasource')
-        if datasource and datasource.endswith('shp'):
-            shp_dir = os.path.abspath(datasource).split('.shp')[0]
-            name = datasource.split('.shp')[0]
-            lyr.datasource = mapnik.Shapefile(file=shp_dir)
-            if options.get('srid'):
-                lyr.srs = '+init=epsg:%s' % options.get('srid')
-                m.srs = lyr.srs
-            else:
-                srs = proj4_from_osr(shp_dir)
-                if srs:
-                    lyr.srs = srs
-    
-        for user_style in layer.UserStyle:
-            for feature_style in user_style.FeatureTypeStyle:
-                m_sty = mapnik.Style()
-                # TODO = Styles should have title,abstract, etc...
-                sty_name = getattr(feature_style,'Name',None)
-                if not sty_name:
-                    sty_name = '%s %s' % (lyr.name,str(idx))
-                sty_name = str(sty_name)
-    
-                for rule in feature_style.Rule:
-                    #print rule.get_childen()
-                    m_rule = mapnik.Rule(str(getattr(rule,'Name','')))
-                    ogc_filter = rule.find("{%s}Filter" % rule.nsmap['ogc'])
-                    if ogc_filter is not None:
-                        # TODO - support ogc:And and oc:Or
-                        m_rule.filter = ogc_filter_to_mapnik(ogc_filter)
-                    else:
-                        if hasattr(rule,'ElseFilter'):
-                              m_rule.set_else(True)
-                    if hasattr(rule,'MaxScaleDenominator'):
-                        m_rule.max_scale = float(rule.MaxScaleDenominator)
-                    if hasattr(rule,'MinScaleDenominator'):
-                        m_rule.min_scale = float(rule.MinScaleDenominator)                    
-                    if hasattr(rule,'LineSymbolizer'):
-                        stroke = rule.LineSymbolizer.Stroke
-                        m_stroke = stroke_to_mapnik(stroke)
-                        m_rule.symbols.append(mapnik.LineSymbolizer(m_stroke))
-                    if hasattr(rule,'PolygonSymbolizer'):
-                        m_poly = mapnik.PolygonSymbolizer()
-                        if hasattr(rule.PolygonSymbolizer,'Fill'):
-                            fill = rule.PolygonSymbolizer.Fill
-                            for css in fill.CssParameter:
-                                if css.get('name') == 'fill':
-                                    m_poly.fill = mapnik.Color(css.text)
-                                elif css.get('name') == 'fill-opacity':
-                                    m_poly.opacity = float(css.text)
-                                else:
-                                    raise Exception('unhanded: ' + css.get('name'))
-                        if hasattr(rule.PolygonSymbolizer,'Stroke'):
-                            stroke = rule.PolygonSymbolizer.Stroke
-                            m_stroke = stroke_to_mapnik(stroke)
-                            m_rule.symbols.append(mapnik.LineSymbolizer(m_stroke))
-                            
-                        m_rule.symbols.append(m_poly)
-                    if hasattr(rule,'PointSymbolizer'):
-                        #fill = rule.PolygonSymbolizer.Fill
-                        #m_point = point_to_mapnik(point)
-                        # TODO
-                        m_rule.symbols.append(mapnik.PointSymbolizer())
-                    if hasattr(rule,'TextSymbolizer'):
-                        text = rule.TextSymbolizer
-                        name = text.Label.find("{%s}PropertyName" % rule.nsmap['ogc'])
-                        if not name and hasattr(text,'Label'):
-                            name = shlex.split(str(text.Label))[0]
+        m_layer, styles = ogc_layer_to_mapnik(layer)
+        for (name, style) in styles:
+            m.append_style(name, style)
+        if datasource:
+            m_layer.datasource = datasource
+        m_layer.srs = m.srs
+        m.layers.append(m_layer)
 
-                        face_name = '[%s]' % text.Font
-
-                        face_name = 'DejaVu Sans Book'
-                        size = 10
-                        for css in text.Font.CssParameter:
-                           if css.get('name') == 'font-family':
-                               face_name = css.text
-                           elif css.get('name') == 'font-size':
-                               size = int(float(css.text))
-                        color = mapnik.Color('black')
-                        for css in text.Fill.CssParameter:
-                            if css.get('name') == 'fill':
-                                color = mapnik.Color(css.text)
-                        m_text = mapnik.TextSymbolizer(mapnik.Expression('['+str(name)+']'),str(face_name),int(size),color)
-                        if hasattr(text,'LabelPlacement'):
-                            if hasattr(text.LabelPlacement,'LinePlacement'):
-                                m_text.label_placement = mapnik.label_placement.LINE_PLACEMENT
-                        if hasattr(text,'Halo'):
-                            h = text.Halo
-                            if hasattr(h,'Radius'):
-                                m_text.halo_radius = float(h.Radius)
-                            if hasattr(h,'Fill'):
-                                for css in h.Fill.CssParameter:
-                                    if css.get('name') == 'fill':
-                                        m_text.halo_fill = mapnik.Color(css.text)
-                            
-                        m_rule.symbols.append(m_text)
-
-                    m_sty.rules.append(m_rule)
-                
-                lyr.styles.append(sty_name)
-                m.append_style(sty_name,m_sty)
-                idx+= 1
-                
-    m.layers.append(lyr)
     if FIX_HEX:
         (handle, path) = tempfile.mkstemp(suffix='.xml', prefix='geotools2mapnik-')
         os.close(handle)
@@ -285,6 +205,161 @@ def main(root,**options):
         print etree.tostring(tree)#,pretty_print=True)
     else:
         print mapnik.save_map_to_string(m)
+
+def ogc_layer_to_mapnik(layer):
+    idx = 0
+    m_layer = mapnik.Layer(str(getattr(layer,'Name',None) or 'Layer'))
+    styles = []
+
+    for user_style in layer.UserStyle:
+        for feature_style in user_style.FeatureTypeStyle:
+            m_sty = mapnik.Style()
+            # TODO = Styles should have title,abstract, etc...
+            sty_name = getattr(feature_style,'Name',None)
+            if not sty_name:
+                sty_name = '%s %s' % (m_layer.name,str(idx))
+            sty_name = str(sty_name)
+
+            for rule in feature_style.Rule:
+                #print rule.get_childen()
+                m_rule = ogc_rule_to_mapnik(rule)
+                m_sty.rules.append(m_rule)
+            
+            m_layer.styles.append(sty_name)
+            styles.append((sty_name, m_sty))
+            idx+= 1
+    return m_layer, styles
+            
+
+def ogc_rule_to_mapnik(rule):
+    m_rule = mapnik.Rule(str(getattr(rule,'Name','')))
+    ogc_filter = rule.find("{%s}Filter" % rule.nsmap['ogc'])
+    if ogc_filter is not None:
+        m_rule.filter = ogc_filter_to_mapnik(ogc_filter)
+    else:
+        if hasattr(rule,'ElseFilter'):
+              m_rule.set_else(True)
+    if hasattr(rule,'MaxScaleDenominator'):
+        m_rule.max_scale = float(rule.MaxScaleDenominator)
+    if hasattr(rule,'MinScaleDenominator'):
+        m_rule.min_scale = float(rule.MinScaleDenominator)                    
+
+    syms = []
+    for possible_sym in rule.iterchildren():
+        if 'Symbolizer' in possible_sym.tag:
+            translator = get_translator(possible_sym)
+            if translator:
+                syms.extend(translator(possible_sym))
+            else:
+                log.warn("Could not translate symbolizer '%s'",
+                         possible_sym.tag)
+    for sym in syms:
+        m_rule.symbols.append(sym)
+
+    return m_rule
+
+_translators = {}
+def translates_sld(tag):
+    def wrap(func):
+        _translators[_sld_tag(tag)] = func
+        return func
+    return wrap
+
+def _sld_tag(name):
+    return "{http://www.opengis.net/sld}"+name
+
+
+def get_translator(node):
+    return _translators.get(node.tag)
+
+
+@translates_sld('LineSymbolizer')
+def ogc_LineSymbolyzer_to_mapnik(sym):
+    stroke = sym.Stroke
+    m_stroke = stroke_to_mapnik(stroke)
+    yield mapnik.LineSymbolizer(m_stroke)
+
+@translates_sld('PolygonSymbolizer')
+def ogc_PolygonSymbolizer_to_mapnik(sym):
+    m_poly = mapnik.PolygonSymbolizer()
+    if hasattr(sym,'Fill'):
+        fill = sym.Fill
+        for css in fill.CssParameter:
+            if css.get('name') == 'fill':
+                m_poly.fill = mapnik.Color(css.text)
+            elif css.get('name') == 'fill-opacity':
+                m_poly.opacity = float(css.text)
+            else:
+                raise Exception('unhanded: ' + css.get('name'))
+    if hasattr(sym,'Stroke'):
+        stroke = sym.Stroke
+        m_stroke = stroke_to_mapnik(stroke)
+        yield mapnik.LineSymbolizer(m_stroke)
+    yield m_poly
+        
+@translates_sld('PointSymbolizer')
+def ogc_PointSymbolizer_to_mapnik(sym):
+    #fill = rule.PolygonSymbolizer.Fill
+    #m_point = point_to_mapnik(point)
+    # TODO
+    yield mapnik.PointSymbolizer()
+
+@translates_sld('TextSymbolizer')
+def ogc_TextSymbolizer_to_mapnik(text):
+    name = text.Label.find("{%s}PropertyName" % rule.nsmap['ogc'])
+    if not name and hasattr(text,'Label'):
+        name = shlex.split(str(text.Label))[0]
+    #face_name = '[%s]' % text.Font
+    face_name = 'DejaVu Sans Book'
+    size = 10
+    for css in text.Font.CssParameter:
+       if css.get('name') == 'font-family':
+           face_name = css.text
+       elif css.get('name') == 'font-size':
+           size = int(float(css.text))
+    color = mapnik.Color('black')
+    for css in text.Fill.CssParameter:
+        if css.get('name') == 'fill':
+            color = mapnik.Color(css.text)
+    m_text = mapnik.TextSymbolizer(
+        mapnik.Expression('[%s]'%name),str(face_name), int(size), color)
+    if hasattr(text,'LabelPlacement'):
+        if hasattr(text.LabelPlacement,'LinePlacement'):
+            m_text.label_placement = mapnik.label_placement.LINE_PLACEMENT
+    if hasattr(text,'Halo'):
+        h = text.Halo
+        if hasattr(h,'Radius'):
+            m_text.halo_radius = float(h.Radius)
+        if hasattr(h,'Fill'):
+            for css in h.Fill.CssParameter:
+                if css.get('name') == 'fill':
+                    m_text.halo_fill = mapnik.Color(css.text)
+    yield m_text
+
+@translates_sld('RasterSymbolizer')
+def ogc_RasterSymbolizer_to_mapnik(sym):
+    if hasattr(sym, 'ColorMap'):
+        m_sym = mapnik.RasterSymbolizer()
+        type = sym.ColorMap.attrib.get('type', 'ramp')
+        type_map = {
+            'ramp': mapnik.COLORIZER_LINEAR,
+            'intervals': mapnik.COLORIZER_DISCRETE,
+            'values': mapnik.COLORIZER_EXACT,
+        }
+        mode = type_map[type]
+        colorizer = mapnik.RasterColorizer(mode, mapnik.Color('transparent'))
+        for c in sym.ColorMap.ColorMapEntry:
+            color = mapnik.Color(c.attrib['color'])
+            color.a = int(float(c.attrib.get('opacity', 1))*255)
+            value = float(c.attrib['quantity'])
+            label = c.attrib.get('label', '')
+            stop = mapnik.ColorizerStop(value, mapnik.COLORIZER_INHERIT, color)
+            stop.label = label
+            colorizer.add_stop(stop)
+        m_sym.colorizer = colorizer
+        yield m_sym
+
+
 
 if __name__ == '__main__':
     parser = optparse.OptionParser(usage="""geotools2mapnik.py <sld.xml> [shapefile] [OPTIONS]""")
